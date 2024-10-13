c5faa2c1a05e707f257d37a7a9ce19a6b9ff1292
gabelm344 <113635032+gabelm344@users.noreply.github.com>
2024-10-13 17:11:31 -0400

    Finished the code in main.py
    Code is finished, but I am not getting correct results. Attempting to debug now.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # main.py
 
-# Under Construction
+from youtube_search import youtube_search
+from display_results import display_results
+
+def main():
+    query = input("Enter a search query: ")
+    results = youtube_search(query)
+    display_results(results)
+
+if __name__ == '__main__':
+    main()
