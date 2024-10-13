4726ad5a437d75ec9bdfd5c0c01a14f10ba78b5a
gabelm344 <113635032+gabelm344@users.noreply.github.com>
2024-10-13 16:08:54 -0400

    Created youtube_search.py and fixed .env
    Completely switched how I wanted to go about the Lab in this commit. Made the .env file only contain the API key and created youtube_search.py to contain the code that will be used for making API requests to YouTube.

---
+++ youtube_search.py
@@ -0,0 +1,28 @@
+# youtube_search.py
+# This file is used to make the function for making API requests to YouTube
+
+import requests
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+api_key = os.getenv('AIzaSyChjwmEWm_VApjJEEOkU4nRgsNIUIFrKFM')
+
+def youtube_search(query):
+    base_url = 'https://www.googleapis.com/youtube/v3/search'
+    params = {
+        'part': 'snippet',
+        'q': query,
+        'type': 'video',
+        'key': api_key,
+        'maxResults': 5
+    }
+
+    response = requests.get(base_url, params=params)
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print(f"Error: Unable to fetch results")
+        return None
