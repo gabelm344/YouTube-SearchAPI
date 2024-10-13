80ad016fda69e9ca8712156e62d8c75826c62fdc
gabelm344 <113635032+gabelm344@users.noreply.github.com>
2024-10-13 15:41:24 -0400

    Updating .env.py
    Added an extra check for the API key. Sends message if API key is not available.

--- .env.py
+++ .env.py
@@ -1,6 +1,12 @@
 from dotenv import load_dotenv
 import os
+
 # Load environment variables from the .env file
 load_dotenv()
+
 # Access environment variables
 api_key = os.getenv('AIzaSyChjwmEWm_VApjJEEOkU4nRgsNIUIFrKFM')
+
+# Making sure API Key works
+if not api_key:
+    print("YouTube API key not found. Please add it to .env file.")
